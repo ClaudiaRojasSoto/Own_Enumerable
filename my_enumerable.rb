a77c42028f4d880095c7d2cc2ed737c74f4fcad7
Cesar Herrera <cesarherr321@gmail.com>
2023-08-23 12:02:13 -0400

    Add class MyList

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,1 +1,13 @@
+require_relative 'my_enumerable'
 
+class MyList
+    include MyEnumerable
+
+    def initialize(*args)
+      @list = args
+    end
+
+    def each
+      @list.each { |element| yield element }
+    end
+end
