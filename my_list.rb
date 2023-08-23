abd9fb3f80b562c6f51493cc0234b4edd653f4f0
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-08-23 12:26:16 -0400

    Create the file my_list to the My_list class where is called the module my_enumerable

--- my_list.rb
+++ my_list.rb
@@ -1,1 +1,20 @@
+require_relative 'my_enumerable'
 
+class MyList
+    include MyEnumerable
+  
+    def initialize(*args)
+      @list = args
+    end
+  
+    def each
+      @list.each { |element| yield element }
+    end
+end
+
+p list = MyList.new(1, 2, 3, 4)
+p list.all? {|e| e < 5}
+p list.all? {|e| e > 5}
+p list.any? {|e| e == 2}
+p list.any? {|e| e == 5}
+p list.filter {|e| e.even?}
